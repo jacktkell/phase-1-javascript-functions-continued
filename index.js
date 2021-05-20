d361f043fa54089097f95fd43b4bbc7414474e83
jack <jack.kelling7@gmail.com>
2021-05-20 14:55:25 -0500

    commit tree 8f0b5218090428feb93298300eeb807bdb4052bb

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code here
+function saturdayFun(event='roller-skate') {
+    return `This Saturday, I want to ${event}!`;
+}
+
+function mondayWork(work='go to the office') {
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(flair = '*') {
+    function encouragingPrompt(prompt = 'special'){
+        return `You are ${flair}${prompt}${flair}`
+    }
+    encouragingPrompt()
+}
